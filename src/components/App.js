3fe08eca01b36929a7b84e5a8f04c997c2abf32e
Spencer Eldred <eldredspencer@gmail.com>
2020-06-13 05:24:48 -1000

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div>
+        App Component
+      </div>
+    )
+  }
+
+}
+
+export default App;
